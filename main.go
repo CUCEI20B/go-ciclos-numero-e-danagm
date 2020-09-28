46bbbab04132ab9278c77fa52fbb60d0bd5da18d
danagm <dana.gomez@alumnos.udg.mx>
2020-09-28 08:08:47 -0500

    prueba numero e 1

--- main.go
+++ main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var e float64 = 0
+	var n float64 = 0
+	var lim float64
+	fmt.Scanln(&lim)
+	for n < lim {
+		var sum float64 = 1
+		var nf float64 = 1
+		for nf < n {
+			sum *= nf
+			nf++
+		}
+		if sum != 0 {
+			e += (1 / sum)
+		}
+		n++
+	}
+
+	fmt.Println(e)
 }
